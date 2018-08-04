9162508f18403f199358fa810812f0b059d81bb4
alex <akourtisdev@gmail.com>
2018-08-04 11:28:07 +0300

    test changes and commit

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 ### You need to start both ports:
 *localhost:3000 & localhost:3001*
 ```
-Go to the react-back folder and type:
+Go to the "server" folder and type:
 
     PORT=3001 npm start 
 ```
 ### Then
 ```
-Go to the client folder and type (from another terminal):
+Go to the "client" folder and type (from another terminal):
     npm start
 ```
 ### Congratulations!
--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  state = { users: []}
+  constructor(props) {
+    super(props);
+    this.state = {
+        email: '',
+        pword: '',
+        users: []
+    }
+}
 
   componentDidMount() {
     fetch('/users')
@@ -11,6 +18,19 @@ class App extends Component {
       .then(users => this.setState({ users }));
   }
 
+signup = e => {
+  // save to db
+}
+
+handleChangeEmail = e => {
+    this.setState({email: e.target.value});
+    console.log(this.state.email)
+}
+
+handleChangePword = e => {
+    this.setState({pword: e.target.value});
+    console.log(this.state.pword)
+}
   render() {
     return (
       <div className="App">
@@ -19,6 +39,29 @@ class App extends Component {
             <li key={user.id}> {user.username}</li>
           )}
         </ul>
+        <form>
+                    <div className="form-group">
+                        <label htmlFor="exampleInputEmail1">Email address</label>
+                        <input 
+                        type="email" 
+                        className="form-control" 
+                        onChange={this.handleChangeEmail} 
+                        id="exampleInputEmail1" 
+                        aria-describedby="emailHelp" 
+                        placeholder="Enter email"/>
+                        <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="exampleInputPassword1">Password</label>
+                        <input 
+                        type="password" 
+                        onChange={this.handleChangePword} 
+                        className="form-control" 
+                        id="exampleInputPassword1" 
+                        placeholder="Password"/>
+                    </div>
+                    <button type="button" onClick={this.signup} className="btn btn-info">Sign up</button>
+                    </form>
       </div>
     );
   }
